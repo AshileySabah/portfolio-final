1d1ff9eb74edc4a0199431d559f2ffa3b54ed4eb
AshileySabah <ashileysabrina2002@gmail.com>
2024-12-05 20:33:24 -0300

    feat: remove background

--- src/pages/Experiencias/styles.tsx
+++ src/pages/Experiencias/styles.tsx
@@ -3,24 +3,4 @@ import styled from "styled-components";
 
 export const Container = styled(Box)`
   min-height: 100vh;
-
-  background: rgb(25, 90, 166);
-  background: -moz-linear-gradient(
-    0deg,
-    rgba(25, 90, 166, 1) 0%,
-    rgba(9, 59, 117, 1) 50%,
-    rgba(4, 42, 87, 1) 100%
-  );
-  background: -webkit-linear-gradient(
-    0deg,
-    rgba(25, 90, 166, 1) 0%,
-    rgba(9, 59, 117, 1) 50%,
-    rgba(4, 42, 87, 1) 100%
-  );
-  background: linear-gradient(
-    0deg,
-    rgba(25, 90, 166, 1) 0%,
-    rgba(9, 59, 117, 1) 50%,
-    rgba(4, 42, 87, 1) 100%
-  );
 `;
