1aa30dfde4dadd1dc3fdc8cc6edda579bcfacb91
AshileySabah <ashileysabrina2002@gmail.com>
2024-12-05 11:52:37 -0300

    feat: habilidades data

---
+++ src/pages/Habilidades/data.ts
@@ -0,0 +1,117 @@
+export const habilidades = [
+  {
+    nicho: "Idiomas",
+    habilidades: [
+      { descricao: "Inglês avançado", imagem: "../../assets/imagem.png" },
+    ],
+  },
+  {
+    nicho: "Frameworks",
+    habilidades: [
+      { descricao: "React", imagem: "../../assets/imagem.png" },
+      { descricao: "Next", imagem: "../../assets/imagem.png" },
+      { descricao: "Angular", imagem: "../../assets/imagem.png" },
+      { descricao: "React Native", imagem: "../../assets/imagem.png" },
+      { descricao: "Flutter", imagem: "../../assets/imagem.png" },
+      { descricao: "Express", imagem: "../../assets/imagem.png" },
+      { descricao: "Spring Boot", imagem: "../../assets/imagem.png" },
+    ],
+  },
+  {
+    nicho: "Linguagens",
+    habilidades: [
+      { descricao: "HTML", imagem: "../../assets/imagem.png" },
+      { descricao: "JavaScript", imagem: "../../assets/imagem.png" },
+      { descricao: "TypeScript", imagem: "../../assets/imagem.png" },
+      { descricao: "Dart", imagem: "../../assets/imagem.png" },
+      { descricao: "Node", imagem: "../../assets/imagem.png" },
+      { descricao: "Java", imagem: "../../assets/imagem.png" },
+    ],
+  },
+  {
+    nicho: "Estilização",
+    habilidades: [
+      { descricao: "CSS", imagem: "../../assets/imagem.png" },
+      { descricao: "SASS", imagem: "../../assets/imagem.png" },
+      { descricao: "Styled Components", imagem: "../../assets/imagem.png" },
+      { descricao: "Material UI", imagem: "../../assets/imagem.png" },
+      { descricao: "Tailwind", imagem: "../../assets/imagem.png" },
+      { descricao: "Bootstrap", imagem: "../../assets/imagem.png" },
+    ],
+  },
+  {
+    nicho: "Banco de Dados",
+    habilidades: [
+      { descricao: "SQL", imagem: "../../assets/imagem.png" },
+      { descricao: "MongoDB", imagem: "../../assets/imagem.png" },
+      { descricao: "MySQL", imagem: "../../assets/imagem.png" },
+      { descricao: "PostgreSQL", imagem: "../../assets/imagem.png" },
+      { descricao: "Firebase", imagem: "../../assets/imagem.png" },
+    ],
+  },
+  {
+    nicho: "Documentação",
+    habilidades: [
+      { descricao: "Storybook", imagem: "../../assets/imagem.png" },
+      { descricao: "Swagger", imagem: "../../assets/imagem.png" },
+    ],
+  },
+  {
+    nicho: "Gerenciador de Estados",
+    habilidades: [
+      { descricao: "Redux", imagem: "../../assets/imagem.png" },
+      { descricao: "Zustand", imagem: "../../assets/imagem.png" },
+    ],
+  },
+  {
+    nicho: "Controle de Versão",
+    habilidades: [{ descricao: "Git", imagem: "../../assets/imagem.png" }],
+  },
+  {
+    nicho: "Teste",
+    habilidades: [
+      { descricao: "Jest", imagem: "../../assets/imagem.png" },
+      { descricao: "JUnit", imagem: "../../assets/imagem.png" },
+    ],
+  },
+  {
+    nicho: "Padrão e Qualidade",
+    habilidades: [
+      { descricao: "ESLint", imagem: "../../assets/imagem.png" },
+      { descricao: "Husky", imagem: "../../assets/imagem.png" },
+      { descricao: "SonarQube", imagem: "../../assets/imagem.png" },
+    ],
+  },
+  {
+    nicho: "Gerência",
+    habilidades: [
+      { descricao: "Liderança de squads", imagem: "../../assets/imagem.png" },
+      {
+        descricao: "Arquitetura de projetos",
+        imagem: "../../assets/imagem.png",
+      },
+      { descricao: "Gestão de tempo", imagem: "../../assets/imagem.png" },
+      {
+        descricao: "Planejamento estratégico",
+        imagem: "../../assets/imagem.png",
+      },
+      {
+        descricao: "Resolução de problemas",
+        imagem: "../../assets/imagem.png",
+      },
+      {
+        descricao: "Colaboração entre equipes",
+        imagem: "../../assets/imagem.png",
+      },
+      {
+        descricao: "Proposta de melhorias técnicas",
+        imagem: "../../assets/imagem.png",
+      },
+      { descricao: "Visão de negócios", imagem: "../../assets/imagem.png" },
+      {
+        descricao: "Análise e antecipação de problemas",
+        imagem: "../../assets/imagem.png",
+      },
+    ],
+  },
+];
