2f907e0cdce861ac3a7d5e05d8c4a908c9e6ec83
AshileySabah <ashileysabrina2002@gmail.com>
2024-12-13 17:53:09 -0300

    feat: contatos - desktop - tooltip

--- src/pages/Contatos/components/Card.tsx
+++ src/pages/Contatos/components/Card.tsx
@@ -1,12 +1,22 @@
+import { Tooltip, useMediaQuery } from "@mui/material";
 import { Contato } from "../data";
 import { Container } from "./styles";
+import { theme } from "@/components/BaseLayout/Theme/styles";
 
 interface CardProps extends Contato {}
 
 export const Card: React.FC<CardProps> = (contato) => {
+  const isScreenSizeUpMd = useMediaQuery(theme?.breakpoints?.up("md"));
+
   return (
-    <Container href={contato?.link}>
-      <img src={contato?.imagem} alt={`Ícone de ${contato?.type}`} />
-    </Container>
+    <Tooltip
+      title={contato?.type}
+      arrow
+      placement={isScreenSizeUpMd ? "right" : "bottom"}
+    >
+      <Container href={contato?.link}>
+        <img src={contato?.imagem} alt={`Ícone de ${contato?.type}`} />
+      </Container>
+    </Tooltip>
   );
 };
