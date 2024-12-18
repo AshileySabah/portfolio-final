ddcbb3837f22e65bbd929dce2f6daeaa796d8548
AshileySabah <ashileysabrina2002@gmail.com>
2024-12-18 18:03:13 -0300

    feat: solucoes button orcar

--- src/pages/Solucoes/components/Card.tsx
+++ src/pages/Solucoes/components/Card.tsx
@@ -1,20 +1,31 @@
-import { SyntheticEvent, useState } from "react";
+import { SyntheticEvent, useEffect, useState } from "react";
 import { AccordionDetails, AccordionSummary, Button } from "@mui/material";
 import { Solucoes } from "../data";
 import { Container, ContainerSummary } from "./styles";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import { useStore } from "../useStore";
 
 interface CardProps extends Solucoes {}
 
 export const Card: React.FC<CardProps> = ({ tipo, descricao }) => {
+  const { setActiveCarousel } = useStore();
   const [expanded, setExpanded] = useState<string | false>(false);
+  const id = tipo?.replaceAll(" ", "");
 
   const handleChange =
     (panel: string) => (_event: SyntheticEvent, isExpanded: boolean) => {
       setExpanded(isExpanded ? panel : false);
     };
 
-  const id = tipo?.replaceAll(" ", "");
+  const handleOrcar = (event: SyntheticEvent) => {
+    event?.stopPropagation();
+  };
+
+  useEffect(() => {
+    if (expanded === tipo) {
+      setActiveCarousel(false);
+    }
+  }, [expanded, tipo]);
 
   return (
     <Container expanded={expanded === tipo} onChange={handleChange(tipo)}>
@@ -25,7 +36,7 @@ export const Card: React.FC<CardProps> = ({ tipo, descricao }) => {
       >
         <ContainerSummary>
           <span>{tipo}</span>
-          <Button variant="outlined" color="secondary">
+          <Button variant="outlined" color="secondary" onClick={handleOrcar}>
             Orçar
           </Button>
         </ContainerSummary>
